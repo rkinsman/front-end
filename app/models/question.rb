b98902f4d7d900270302ab596d8f70bb78862de7
Dylan Lederle-Ensign <dylan.lederle.ensign@gmail.com>
2012-10-17 21:52:25 -0400

    Add field for learners answer

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,17 @@
 class Question 
   include Mongoid::Document  
   attr_protected :qText, :correctAnswer, :aText 
+  before_create :default_values
+
   field :qText, type: String
   field :correctAnswer, type: Boolean
   field :aText, type: String
+  field :learnerAnswer, type: String
   belongs_to :lesson, :class_name => "Lesson", :inverse_of => :questions
 
+
+  private
+    def default_values
+      self.learnerAnswer ||= "U"
+    end
 end
